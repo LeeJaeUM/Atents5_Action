4c0a7b3205e162b62d8e374c40bcc4bd4a91d823
LeeJaeUng <lsam3333333@gmail.com>
2024-03-07 13:11:58 +0900

    기본 idle 서브 스테이트머신 추가

---
+++ Assets/Scripts/AnimationScripts/RandomIdleSelector.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomIdleSelector : StateMachineBehaviour
+{
+    readonly int IdleSelect_Hash = Animator.StringToHash("IdleSelect");
+
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        // 랜덤한 값을 생성하여 IdleSelect에 할당
+        animator.SetInteger(IdleSelect_Hash, RandomSelect());
+    }
+
+    int RandomSelect()
+    {
+        int select = 0;
+        float num = Random.value;
+
+        if(num < 0.05f)
+        {
+            select = 4;
+        }
+        else if(num < 0.15f)
+        {
+            select = 3;
+        }
+        else if(num < 0.3f)
+        {
+            select = 2;
+        }
+        else if(num < 0.45f)
+        {
+            select = 1;
+        }
+        else
+        {
+            select = 0;
+        }
+        return select;
+    }
+}
